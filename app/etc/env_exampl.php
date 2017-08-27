8f7244a3103e007f3b9284b4f79a9758b5de0ac7
Maks <ffwmakssherbyna@gmail.com>
2017-08-27 22:29:23 +0300

    add dump database and config example

---
+++ app/etc/env_exampl.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '524f2d25e61dd3c2cf3a8f460e805005',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => 'user',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sat, 26 Aug 2017 12:48:55 +0000',
+  ),
+);
